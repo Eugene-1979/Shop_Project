0a18522954e52eec42924e61fdde9b2ec2072ad6
Eugene <80662982619q@gmail.com>
2023-01-29 20:30:47 +0200

    Добавьте файлы проекта.

---
+++ Db/AppDbContent.cs
@@ -0,0 +1,57 @@
+﻿using Shop_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Shop_Project.Db
+    {
+    public class AppDbContent : DbContext
+        {
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+            modelBuilder
+               .Entity<Product>()
+               .HasMany(c => c.Orders)
+               .WithMany(s => s.Products)
+               .UsingEntity<Enrollment>(
+                  j => j
+
+
+                   .HasOne(pt => pt.Order)
+                   .WithMany(t => t.Enrollments)
+                   .HasForeignKey(pt => pt.OrderId),
+
+               j => j
+                   .HasOne(pt => pt.Product)
+                   .WithMany(p => p.Enrollments)
+                   .HasForeignKey(pt => pt.ProductId),
+               j =>
+               {
+
+                   j.Property(pt => pt.Count).HasDefaultValue(3);
+                   j.HasKey(t => new { t.OrderId, t.ProductId });
+                   j.ToTable("Enrollments");
+               });
+
+            } 
+
+
+
+
+        public AppDbContent(DbContextOptions<AppDbContent> options) : base(options)
+            {
+
+
+       
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Shop_Project.Models.Enrollment> Enrollment { get; set; } = default!;
+   
+
+        }
+    }
---
+++ Repository/ProductRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop_Project.Db;
+using Shop_Project.Models;
+
+namespace Shop_Project.Repository
+    {
+    public class ProductRepository : IReposytory<Product>
+        {
+        internal readonly AppDbContent _context;
+
+        public ProductRepository(AppDbContent context)
+            {
+            _context = context;
+            }
+
+
+
+
+      async  public Task ModelAddAsync(Product model)
+            {
+            _context.Add(model);
+            await _context.SaveChangesAsync();
+            }
+
+      async  public Task<IEnumerable<Product>> ModelAllAsync()=>await _context.Products.Include(p => p.Category).ToListAsync();
+
+
+      async  public Task ModelDeleteAsync(Product model)
+            {
+            if(model != null)
+                {
+                _context.Products.Remove(model);
+                }
+
+            await _context.SaveChangesAsync();
+            }
+
+        public bool ModelExist(int id)=> (_context.Products?.Any(e => e.Id == id)).GetValueOrDefault();
+
+
+       async public Task<Product> ModelFirstofDefaultAsync(int? id)=> await _context.Products
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+
+       async public Task<Product> ModelIdAsync(int? id)=> await _context.Products.FindAsync(id);
+
+
+       async public Task ModelUpdateAsync(Product model)
+            {
+            _context.Update(model);
+            await _context.SaveChangesAsync();
+            }
+
+        public (bool,string) CheckModel(Product model)
+            {
+
+            if((model.Sale < ShopRepository._minSale) || (model.Sale > ShopRepository._maxSale)) return (false, "incorrect sale");
+            if(_context.Products.ToList().Contains(model)) return (false, "Product is Exist");
+
+             
+            return (true, "ok");
+
+        }
+
+        }
+    }
